2ffe282132336e15ec232a8205917219ce9afd00
Jaden175 <40519054+Jaden175@users.noreply.github.com>
2018-12-06 21:10:31 -0500

    Full Upload
    Full Upload

---
+++ Assignment 2/categories_nav.php
@@ -0,0 +1,26 @@
+<!-- Jaden Hargraves -->
+<!-- IT 031 -->
+<!-- Assignment 2 -->
+<main>
+    <h1>Product List</h1>
+
+    <aside>
+        <!-- display a list of categories -->
+        <h2>Categories</h2>
+        <nav>
+        <ul>
+        <?php foreach ($categories as $category) : ?>
+            <li>
+            <a href="?category_id=<?php echo $category['categoryID']; ?>">
+                <?php echo $category['categoryName']; ?>
+            </a>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+        </nav>
+    </aside>
+
+</main>
+<!-- Jaden Hargraves -->
+<!-- IT 031 -->
+<!-- Assignment 2 -->	
